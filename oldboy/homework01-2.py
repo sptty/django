1bdb2232e9b7fc3b39544fa2c07082da95316661
sptty <747824617@qq.com>
2016-10-19 14:41:45 +0800

    ready to do day 01 homework02

---
+++ oldboy/homework01-2.py
@@ -0,0 +1,14 @@
+# -*- encoding:utf-8 -*-
+#
+#
+#     要求：
+#     1.开发员工信息查询程序，可以支持模糊查询员工信息
+#     2.匹配到的员工信息，需高亮显示，并且显示匹配条数
+#     3.支持员工信息的增删改查
+#     4.用户在启动程序后，可以不断的循环查询
+#
+#     信息表素材：staff.txt
+#     Alex Li, Engineer, IT, 13651054608, alex@126.com
+#     Jack Ma, Salesman, Sales Dep., 18546304334, jack@sina.com
+#     Rain Wang, HR, HR Dep., 13345634253, rain@autohome.com.cn
+
