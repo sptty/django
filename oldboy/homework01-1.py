8910b938eaca1dfb04a797e5abef8cec73f3259d
sptty <747824617@qq.com>
2016-10-21 17:16:29 +0800

    ready to do day 01 homework02

--- oldboy/homework01-1.py
+++ oldboy/homework01-1.py
@@ -1,12 +1,12 @@
 # -*- coding:utf-8 -*-
 
-import os,sys,re
+import os,sys,re,getpass
 
 
 def info_index():
     __doc__ = '输入用户名和密码'
     name=raw_input('Input your name:')
-    password=raw_input('Input your password:')
+    password=getpass.getpass('Input your password:')
     new_info_user = str(name+','+password)
     return new_info_user
 
@@ -14,7 +14,7 @@ def info_index():
 '''
 def all_user_name_list():
     correct_name = []
-    with open('./day01_databases.txt') as user_databases:
+    with open('./day01_user_name_password.txt') as user_databases:
         for line in user_databases.readlines():
             # print(line)
             correct_name.append(line.split(',')[0])
@@ -30,7 +30,7 @@ def all_user_name_list():
 
 def all_user_name_password_times_list():
     __doc__ = '获取文件中所有的正确的用户名密码和输入错误次数'
-    with open('./day01_databases.txt') as user_databases:
+    with open('./day01_user_name_password.txt') as user_databases:
         correct_name_password_times = []
         for line in user_databases.readlines():
             correct_name_password_times.append(line)          # 这里的correct_name_password_times列表，但是调用的时候，return的整个列表值就变成了元组中的第一个元素，
@@ -41,24 +41,23 @@ def all_user_name_password_times_list():
         return correct_name_password_times
 
 
-# def write_user_databases(*correct_name_password_times):
-#     __doc__ = '将输入密码的用户，记录他们输错的次数，并写入文件'
-#     for i in correct_name_password_times:  # 这一步的作用跟之前一样，凡是函数传递的元组都这样处理
-#         # print('i  '+ str(i)+ str(type(i)))
-#         correct_name_password_times = list(i)
-#         # print('correct_name_password_times  '+ str(correct_name_password_times)+ str(type(correct_name_password_times)))
-#
-#     for i in range(0, len(correct_name_password_times)):
-#         #  将用户信息逐行写入新文件，在替换原来原件
-#         user_databases = open('./.day01_databases.txt.tmp', 'a')
-#         # print(str(correct_name_password_times[i]))
-#         user_databases.write(correct_name_password_times[i])
-#         user_databases.close()
-#         # else:
-#         # user_databases = open('./.day01_databases.txt.tmp', 'w')
-#     os.remove('./day01_databases.txt')
-#     os.rename('./.day01_databases.txt.tmp', './day01_databases.txt')
+def write_user_databases(*correct_name_password_times):
+    __doc__ = '将输入密码的用户，记录他们输错的次数，并写入文件'
+    for i in correct_name_password_times:  # 这一步的作用跟之前一样，凡是函数传递的元组都这样处理
+        # print('i  '+ str(i)+ str(type(i)))
+        correct_name_password_times = list(i)
+        # print('correct_name_password_times  '+ str(correct_name_password_times)+ str(type(correct_name_password_times)))
 
+    for i in range(0, len(correct_name_password_times)):
+        #  将用户信息逐行写入新文件，在替换原来原件
+        user_databases = open('./.day01_user_name_password.txt.tmp', 'a')
+        # print(str(correct_name_password_times[i]))
+        user_databases.write(correct_name_password_times[i])
+        user_databases.close()
+        # else:
+        # user_databases = open('./.day01_user_name_password.txt.tmp', 'w')
+    os.remove('./day01_user_name_password.txt')
+    os.rename('./.day01_user_name_password.txt.tmp', './day01_user_name_password.txt')
 
 def judge_user_name(new_info_user,*correct_name_password_times):
     __doc__ = '判断用户名，密码和输入次数是否禁止登录'
@@ -94,23 +93,7 @@ def judge_user_name(new_info_user,*correct_name_password_times):
         # print('the user ' + new_name + ' is not  exist 1111111!')
         new_name_status=1
 
-    def write_user_databases(*correct_name_password_times):
-        __doc__='将输入密码的用户，记录他们输错的次数，并写入文件'
-        for i in correct_name_password_times:   # 这一步的作用跟之前一样，凡是函数传递的元组都这样处理
-            # print('i  '+ str(i)+ str(type(i)))
-            correct_name_password_times=list(i)
-            # print('correct_name_password_times  '+ str(correct_name_password_times)+ str(type(correct_name_password_times)))
 
-        for i in range(0, len(correct_name_password_times)):
-            #  将用户信息逐行写入新文件，在替换原来原件
-            user_databases = open('./.day01_databases.txt.tmp', 'a')
-            # print(str(correct_name_password_times[i]))
-            user_databases.write(correct_name_password_times[i])
-            user_databases.close()
-            # else:
-                # user_databases = open('./.day01_databases.txt.tmp', 'w')
-        os.remove('./day01_databases.txt')
-        os.rename('./.day01_databases.txt.tmp','./day01_databases.txt')
 
 
     if new_name_status==0:
@@ -124,9 +107,16 @@ def judge_user_name(new_info_user,*correct_name_password_times):
                 # print('the user ' + new_name + ' is  exist !')
                 # print(correct_name+correct_password+correct_times)
                 if int(correct_times) >=3:
-                    print(new_name + ' forbidden to login!')
+                    print(new_name + ' forbidden to login!,Do you want to ban the user?(YES/NO)')
+                    CHOICE = raw_input('Input your Choice:')
+                    if CHOICE == 'YES':
+                        correct_times=0
+                        correct_name_password_times[i]=correct_name+','+correct_password+','+str(correct_times)+'\n'
+                        print(correct_name+' has been banned!')
+                        return correct_name_password_times
                 elif new_password == correct_password:
                     print('welcome' + ',' + new_name + ', login successful!!')
+                    return None
                 else:
                     print('the user ' + new_name + ' password is not correct !')
                     correct_times=int(correct_times)+1
@@ -134,14 +124,18 @@ def judge_user_name(new_info_user,*correct_name_password_times):
                     # print('correct_name_password_times[i]'+correct_name_password_times[i])
                     # password_is_not_correct=0
                     # print('correct_times is ' + str(correct_times))
-
-                    # return new_name+correct_times
-                    write_user_databases(correct_name_password_times)
+                    return correct_name_password_times
+                    # write_user_databases(correct_name_password_times)
 
                 # else:
             #     print('the user ' + new_name + ' is not  exist !')
-    else:
-        print('the user ' + new_name + ' is not  exist, system is logining out ......')
+    elif new_name_status==1:
+        print('the user ' + new_name + ' is not  exist, system is logining out ......\nDo you want to register ?(YES/NO)')
+        CHOICE = raw_input('Input your Choice:')
+        if CHOICE=='YES':
+            return None
+
+
         # 提示用户不存在，退出系统
 
 
@@ -172,7 +166,7 @@ def judge_user_password(new_info_user,correct_name_password):
 
 
 def forbidden_user_login(new_info_user,new_name):
-    with open('day01_databases.txt','a') as f:
+    with open('day01_user_name_password.txt','a') as f:
         line = f.readlines()
         for i in line:
             correct_name,correct_password,count=i.split(',')
@@ -190,9 +184,25 @@ def forbidden_user_login(new_info_user,new_name):
 
 '''
 
+def register_user(*new_info_user):
+    __doc__ = '将输入密码的用户，记录他们输错的次数，并写入文件'
+    for i in new_info_user:  # 这一步的作用跟之前一样，凡是函数传递的元组都这样处理
+        # print('i  '+ str(i)+ str(type(i)))
+        new_info_user = str(i)
+        # print('correct_name_password_times  '+ str(correct_name_password_times)+ str(type(correct_name_password_times)))
+    new_info_user=new_info_user+',0'+'\n'
+    #  将用户信息写入文件
+    user_databases = open('./day01_user_name_password.txt', 'a+')
+    # print(str(correct_name_password_times[i]))
+    user_databases.write(new_info_user)
+    user_databases.close()
+
 
 if __name__=='__main__':
     new_info_user=info_index()
+    print(new_info_user)
+
+
     # correct_name=all_user_name_list()
     correct_name_password_times=all_user_name_password_times_list()
     # print('correct_name_password_times' + ' is ' + str(type(correct_name_password_times)) + str(correct_name_password_times))
@@ -201,8 +211,14 @@ if __name__=='__main__':
 
     judge_user_name_results=judge_user_name(new_info_user,correct_name_password_times)
 
-    # print('judge_user_name_results'+' is '+str(type(judge_user_name_results))+str(judge_user_name_results))
-    # forbidden_user_login(new_info_user,correct_name)
+
+    if judge_user_name_results is not None:
+        write_user_databases(judge_user_name_results)
+
+    elif judge_user_name_results is None:
+        new_info_user=info_index()
+        print(new_info_user)
+        register_user(new_info_user)
 
 '''
     if judge_user_name_results is False:
--- oldboy/homework01-2.py
+++ oldboy/homework01-2.py
@@ -1,6 +1,5 @@
 # -*- encoding:utf-8 -*-
 #
-#
 #     要求：
 #     1.开发员工信息查询程序，可以支持模糊查询员工信息
 #     2.匹配到的员工信息，需高亮显示，并且显示匹配条数
@@ -8,7 +7,140 @@
 #     4.用户在启动程序后，可以不断的循环查询
 #
 #     信息表素材：staff.txt
-#     Alex Li, Engineer, IT, 13651054608, alex@126.com
-#     Jack Ma, Salesman, Sales Dep., 18546304334, jack@sina.com
-#     Rain Wang, HR, HR Dep., 13345634253, rain@autohome.com.cn
+
+import os
+
+def user_choice():
+
+    def user_choice_input(choice_num):
+        try:
+            count=0
+            while not (int(choice_num) >= 1 and int(choice_num) <= 5):
+                choice_num=raw_input(u'输入错误,请输入数字,您的选择序号(0-5):')
+                count+=1
+                if count>=3:
+                    choice_num=5
+                    print('您输错次数已超过3次，系统即将退出......')
+                    return choice_num
+        except ValueError:
+            print('您的输入非数字，系统即将退出......')
+            choice_num=5
+
+        return choice_num
+
+    print('员工查询系统菜单：\n1.增加员工信息\n2.修改员工信息\n3.查询员工信息\n4.删除员工信息\n5.退出查询系统')
+
+    choice_num = raw_input(u'请输入您的选择序号(0-5):')
+    choice_num=user_choice_input(choice_num)
+    return choice_num
+
+
+def input_staff_info():
+    new_staff_name = raw_input(u'请输入姓名：  ')
+    new_staff_job = raw_input(u'请输入职称:  ')
+    new_staff_dept = raw_input(u'请输入部门:  ')
+    new_staff_tel = raw_input(u'请输入手机号码:  ')
+    new_staff_email = raw_input(u'请输入电子邮箱:  ')
+    new_staff_info=new_staff_name + ',' + new_staff_job + ',' + new_staff_dept + ',' + new_staff_tel + ',' + new_staff_email+'\n'
+    return new_staff_info
+
+
+def all_staff_info():
+    all_staff_info_list=[]
+    with open('./day01_user_info.txt','r') as  all_staff_info:
+        for i in all_staff_info.readlines():
+            print(i)
+            all_staff_info_list.append(i)
+
+    return all_staff_info_list
+
+
+def new_all_staff_info(*extra_staff_info):
+        # all_staff_info_list = []
+        with open('./day01_user_info.txt', 'r') as  all_staff_info:
+            all_staff_info_list=list(all_staff_info)
+            # print('all_staff_info_list:' + str(all_staff_info_list) + str(type(all_staff_info_list)))
+            for i in extra_staff_info:
+                all_staff_info_list.append(i+'\n')
+            # print('all_staff_info_list'+str(type(all_staff_info_list)))
+                return all_staff_info_list
+
+
+def write_to_files(*new_all_staff_info):
+
+    with open('./.day01_user_info.txt.tmp', 'a') as  all_staff_info_tmp:
+        for i in new_all_staff_info:
+            for j in i:
+                all_staff_info_tmp.write(j)
+    all_staff_info_tmp.close()
+
+    os.remove('./day01_user_info.txt')
+    os.rename('./.day01_user_info.txt.tmp', './day01_user_info.txt')
+
+
+def staff_search():
+    pass
+
+def staff_modify():
+    pass
+
+def staff_delete():
+    pass
+
+def staff_display():
+    pass
+
+def staff_info():
+    pass
+
+def modify_staff_info():
+    pass
+
+def exit_judge(user_choice):
+    if int(user_choice) == 5:
+        return True
+    else:
+        # print('不退出执行')
+        return False
+
+
+
+if __name__=='__main__':
+    print('----------------Staff information query system-------------------')
+
+    exit_status=False
+
+    while not exit_status:                           # 循环执行
+
+        user_choice_num = int(user_choice())             # 获取用户输入的数字
+        EXIT_JUDGE = exit_judge(user_choice_num)     # 判断是否退出,值为True和False
+        if EXIT_JUDGE:
+            break
+
+        if   user_choice_num == 1:
+            print('1.增加员工信息')
+            new_staff_info=input_staff_info()
+
+            all_staff_info_list=all_staff_info()
+            if new_staff_info in all_staff_info_list:
+                print('The information of new staff '+ new_staff_info.split(',')[0]+' is exist.Please re-inout new staff information.')
+            else:
+                new_all_staff_info_list=new_all_staff_info(new_staff_info)
+                write_to_files(new_all_staff_info_list)
+
+        elif user_choice_num == 2:
+            print('2.修改员工信息')
+
+        elif user_choice_num == 3:
+            print('3.查询员工信息')
+
+        elif user_choice_num == 4:
+            print('4.删除员工信息')
+
+
+
+
+    print('-----------------------Logging out system------------------------')
+
+
 
